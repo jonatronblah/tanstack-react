327637dbdd6fa008be49ca0012e23b0e12ee7260
unknown <jonatron@gmail.com>
2024-06-02 01:57:29 -0400

    m

--- app-client/src/routes/index.lazy.tsx
+++ app-client/src/routes/index.lazy.tsx
@@ -1,13 +1,17 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/')({
+import { ContentLayout } from "@/components/layouts/layout";
+
+export const Route = createLazyFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+    <ContentLayout title="Home">
+      <div className="p-2">
+        <h3>Welcome Home!</h3>
+      </div>
+    </ContentLayout>
+  );
 }
--- app-client/src/routes/stuff.lazy.tsx
+++ app-client/src/routes/stuff.lazy.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-
 import { createLazyFileRoute } from "@tanstack/react-router";
-
 import { Calendar } from "@/components/ui/calendar";
+
+import { ContentLayout } from "@/components/layouts/layout";
 
 export const Route = createLazyFileRoute("/stuff")({
   component: Stuff,
@@ -12,11 +12,13 @@ function Stuff() {
   const [date, setDate] = useState<Date | undefined>(new Date());
 
   return (
-    <Calendar
-      mode="single"
-      selected={date}
-      onSelect={setDate}
-      className="rounded-md border"
-    />
+    <ContentLayout title="Calendar">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </ContentLayout>
   );
 }
