4f01da7637f8e46b52a3b613de097924d2772a2c
unknown <jonatronblah@github.com>
2024-06-08 12:34:07 -0400

    stuff

--- client/src/routes/about.lazy.tsx
+++ client/src/routes/about.lazy.tsx
@@ -1,9 +1,13 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 
+import { SingleFileUploader } from '@components/functions/upload.tsx'
+
 export const Route = createLazyFileRoute('/about')({
   component: About,
 })
 
 function About() {
-  return <div className="p-2">Hello from About!</div>
+  return <div>
+    <SingleFileUploader />
+  </div>
 }
