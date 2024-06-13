d4afbcb91c664e4aab51d7b8129cc4e3c7b1f913
unknown <jonatron@gmail.com>
2024-06-13 14:26:19 -0400

    fetch working

--- client/src/components/functions/upload.tsx
+++ client/src/components/functions/upload.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
-import { useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { fetchAsync } from "@/lib/api-client";
 
 export const SingleFileUploader = () => {
+  const { status, data, error } = useQuery({
+    queryKey: ["msg", "hello"],
+    queryFn: fetchAsync,
+  });
 
-  const {status, data, error}  = useQuery({ queryKey: ['hello'], queryFn: fetchAsync })
-
-  return (
-    <>
-      {data}
-    </>
-  );
+  return <>{data.message}</>;
 };
-
--- client/src/lib/api-client.ts
+++ client/src/lib/api-client.ts
@@ -1,8 +1,10 @@
 
 
 
-export const fetchAsync = async () => {
-    const response = await fetch('/api/v1/hello')
+export const fetchAsync = async ({ queryKey }) => {
+    const [_, msg] = queryKey
+    console.log(msg)
+    const response = await fetch(`/api/v1/${msg}`)
     if (!response.ok) {
         throw new Error('Network response was not ok')
     }
